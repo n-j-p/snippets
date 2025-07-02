9d6d9e4cc38a340a6bb94d2f22e2575d84143393
n-j-p <10066183+n-j-p@users.noreply.github.com>
2025-07-02 11:18:29 +1000

    Update tryCatch_example.R

--- tryCatch_example.R
+++ tryCatch_example.R
@@ -1,20 +1,15 @@
-f <- function(x) sqrt(x)
-
-safef <- function(x) {
-  tryCatch(
+f = function(x) sqrt(x)
+f2 <- function(x) tryCatch(
     expr = {
-        ans = f(x)
-    },
-    error = function(e){ 
-        # (Optional)
-        # Do this if an error is caught...
-    },
-    warning = function(w){
-      stop('square root of negative number')
-    },
-    finally = {
-        # (Optional)
-        # Do this at the end before quitting the tryCatch structure...
+        f(x)
+    }, 
+    warning = function(cond){
+        print('negative x')
+        print(conditionMessage(cond))
+        NULL
     }
-  )
-}
+)
+
+f2(2)
+f2(-2)
+
